b7b4768e8ae3470da69578a58002c3ea4df91d4d
adammKass <adam.kascak@protonmail.com>
2025-10-09 00:52:55 +0200

    -stars fix, landscape fix

--- src/js/starsLoop.js
+++ src/js/starsLoop.js
@@ -4,7 +4,7 @@ const starCount = 5;
 containers.forEach((div) => {
   for (let i = 0; i < starCount; i++) {
     const star = document.createElement("img");
-    star.src = "../../public/svg/star.svg";
+    star.src = "../public/svg/star.svg";
     star.alt = "Hodnotenie";
     star.classList.add("w-4", "h-4");
     div.appendChild(star);
